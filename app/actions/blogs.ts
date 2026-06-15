35707a414d0343907864fdb51367098ddd9e07d8
Mahmud Hossain Sushmoy <sushmoy@duck.com>
2026-06-15 13:03:28 +0600

    13. Validations in blog creation using useActionState so that the server     can communicate with the client side state and update it

--- app/actions/blogs.ts
+++ app/actions/blogs.ts
@@ -3,13 +3,28 @@
 import { revalidatePath } from "next/cache";
 import { addBlog, addLike } from "../services/blogs";
 import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 
-export const createBlog = async (formData: FormData) => {
+export const createBlog = async (
+  prevState: { error: string },
+  formData: FormData,
+) => {
+  const session = await auth();
+  if (!session) {
+    redirect("/login");
+  }
+
   const blog = {
     title: formData.get("title") as string,
     author: formData.get("author") as string,
     url: formData.get("url") as string,
   };
+
+  if (blog.title.length < 5 || blog.author.length < 5 || blog.url.length < 5) {
+    return {
+      error: "title, author, and url must be of at least 5 characters long",
+    };
+  }
   await addBlog(blog);
 
   revalidatePath("/blogs");
--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -1,19 +1,44 @@
+"use client";
+
 import { createBlog } from "@/app/actions/blogs";
+import { useActionState } from "react";
 
 const NewBlog = () => {
+  const [state, formAction] = useActionState(createBlog, { error: "" });
   return (
     <div>
-      <form action={createBlog}>
+      <form action={formAction}>
         <div>
-          <input type="text" name="title" placeholder="title"  className="inp"/>
+          <input
+            type="text"
+            name="title"
+            placeholder="title"
+            className="inp"
+            minLength={5}
+          />
         </div>
         <div>
-          <input type="text" name="author" placeholder="author"  className="inp"/>
+          <input
+            type="text"
+            name="author"
+            placeholder="author"
+            className="inp"
+            minLength={5}
+          />
         </div>
         <div>
-          <input type="text" name="url" placeholder="url"  className="inp"/>
+          <input
+            type="text"
+            name="url"
+            placeholder="url"
+            className="inp"
+            minLength={5}
+          />
         </div>
-        <button type="submit" className="btn">Submit</button>
+        <button type="submit" className="btn">
+          Submit
+        </button>
+        {state && <p className="text-red-600">{state.error}</p>}
       </form>
     </div>
   );
