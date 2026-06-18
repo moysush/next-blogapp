27b8bd341af12fc44edab36c24bd2b37ccf171ad
Mahmud Hossain Sushmoy <sushmoy@duck.com>
2026-06-18 16:36:39 +0600

    22. Static homepage from markdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import HomePage from "./homepage.mdx";
 export default function Home() {
-  return <div className="text-2xl">Welcome to the Blogapp!</div>;
+  return (
+    <div className="markdown">
+      <HomePage />
+    </div>
+  );
 }
